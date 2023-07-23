1d268c1afd7138fc4704f19c35553cf4a01b8ed1
unmati3 <105388675+unmati3@users.noreply.github.com>
2023-07-23 19:40:57 -0400

    Initial commit

---
+++ Console_War.py
@@ -0,0 +1,22 @@
+﻿import time
+def Start():
+    print("Welcome to...")
+def Title():
+    print("   ____ ___  _   _ ____   ___  _     _____  __        ___    ____   ")
+    print("  / ___/ _ \| \ | / ___| / _ \| |   | ____| \ \      / / \  |  _ \  ")
+    print(" | |  | | | |  \| \___ \| | | | |   |  _|    \ \ /\ / / _ \ | |_) | ")
+    print(" | |__| |_| | |\  |___) | |_| | |___| |___    \ V  V / ___ \|  _ <  ")
+    print("  \____\___/|_| \_|____/ \___/|_____|_____|    \_/\_/_/   \_\_| \_\ (fullscreen)")
+Start()
+time.sleep(2)
+Title()
+Life = 100
+exper = 0
+PlName = input("Insert your name -->")
+print("Welcome to this adventure", PlName)
+print("You have ❤️",Life, "life")
+print("You have 🟢",exper, "xp")
+ContAdv = input("Do you want to continue this adventure (y/n)")
+print("In a city so far far away is a couple of brothers named Joe and Jack who they are thieves and plans bad things for the city...")
+time.sleep(5)
+print("Fortunately", PlName, "protects the city against this bad couple!")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Console-War
+ lil project
